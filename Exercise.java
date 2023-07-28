08aa50f1625ccb41d0abf2977de53b9f0a584b7e
Mahabir <mahabirg81@gmail.com>
2023-07-28 14:36:16 +0800

    implement method

--- Exercise.java
+++ Exercise.java
@@ -3,28 +3,27 @@ import java.util.Arrays;
 public class Exercise {
 
     public int maxProfit(int[] prices) {
-        int firstHighest = Integer.MIN_VALUE;
-        int secondHighest = Integer.MIN_VALUE;
-//
-        for (int score : array) {
-            System.out.println(score);
-            if (score > firstHighest) {
-                secondHighest = firstHighest;
-                firstHighest = score;
-            } else if (score > secondHighest && score < firstHighest) {
-                secondHighest = score;
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int dayPrice : prices) {
+            System.out.println(dayPrice);
+            if (dayPrice < buyPrice) {
+                buyPrice = dayPrice;
+            } else if (dayPrice - buyPrice > maxProfit) {
+                maxProfit = dayPrice - buyPrice;
             }
         }
-
-        System.out.println(Arrays.toString(new int[]{firstHighest, secondHighest}));
-        return new int[]{firstHighest, secondHighest};
+        System.out.println("Buying price is $" + buyPrice +" and selling price is $" + (buyPrice + maxProfit));
+        System.out.println("Maximum profit is = $" +maxProfit);
+        return maxProfit;
 
     }
 
     public static void main(String[] args) {
-        int[] array =  new int[]{84,85,86,87,85,90,85,83,23,45,84,1,2,0};
+        int[] prices =  new int[]{7, 1, 5, 3, 6, 4};
         Exercise exercise = new Exercise( );
-        exercise.maxProfit(array);
+        exercise.maxProfit(prices);
 
 
     }
